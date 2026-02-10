364496d744ad2245b0acfd6550f406c56bc825c2
Wesley Pereira <wessil170@gmail.com>
2026-02-10 15:57:37 -0300

    fix: troca bcrypt por pbkdf2 (python 3.13 compat)

--- security.py
+++ security.py
@@ -1,9 +1,13 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"])
+pwd_context = CryptContext(
+    schemes=["pbkdf2_sha256"],
+    deprecated="auto"
+)
 
-def criar_hash(senha: str):
+def criar_hash(senha: str) -> str:
     return pwd_context.hash(senha)
 
-def verificar_senha(senha_digitada: str, senha_hash: str):
-    return pwd_context.verify(senha_digitada, senha_hash)
+def verificar_senha(senha: str, senha_hash: str) -> bool:
+    return pwd_context.verify(senha, senha_hash)
+
